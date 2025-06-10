0df09902c71e2c017da7a870c8fc55c0f0a7c79a
Christos <baharidis13@gmail.com>
2025-06-10 20:03:37 +0200

    modified v2

--- person.py
+++ person.py
@@ -1,10 +1,9 @@
 class Person:
-    def __init__(self, id, first_name, last_name, date_of_birth, description):
+    def __init__(self, id, first_name, last_name, date_of_birth):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
-        self.description = description
         
         # Κενά relationships στην αρχή
         self.spouse = None
@@ -12,4 +11,4 @@ class Person:
         self.parents = []
     
     def as_dictionary(self):
-        return {'first_name':self.first_name, 'last_name':self.last_name, 'id': self.id, 'birth_date': self.date_of_birth, 'description': self.description}
+        return {'first_name':self.first_name, 'last_name':self.last_name, 'id': self.id, 'birth_date': self.date_of_birth}
